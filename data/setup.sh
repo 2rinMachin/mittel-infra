b6e97fb69d28bf3f929edd9df57fb5cd861d4234
José Daniel Grayson <josedanielgrayson@proton.me>
2025-09-26 23:32:11 -0500

    feat: add strict mode to setup.sh and remove dangling awscli installation files

--- data/setup.sh
+++ data/setup.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+IFS=$'\n\t'
+
+touch ~/.hushlogin
+
 sudo apt update
 sudo apt install -y unzip
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+rm -rf ./aws ./awscliv2.zip
 
 # Add Docker's official GPG key:
 sudo apt update
