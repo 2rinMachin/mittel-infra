a94d116d249d3c3a9cf804369776f5a80f19373e
José Daniel Grayson <josedanielgrayson@proton.me>
2025-10-05 20:29:51 -0500

    fix: fix amplify app redirection rules

--- main.tf
+++ main.tf
@@ -624,7 +624,7 @@ resource "aws_amplify_app" "frontend" {
   }
 
   custom_rule {
-    source = "/<*>"
+    source = "</^[^.]+$|\\.(?!(css|gif|ico|jpg|jpeg|js|png|mov|webm|webmanifest|txt|svg|woff|woff2|ttf|map|json|webp)$)([^.]+$)/>"
     target = "/index.html"
     status = "200"
   }
